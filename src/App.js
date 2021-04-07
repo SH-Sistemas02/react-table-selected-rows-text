56fe87900a08447928a675d69bec6c204dceddba
SH-Sistemas02 <71289626+SH-Sistemas02@users.noreply.github.com>
2021-04-07 16:25:02 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,245 @@
+import React from "react";
+import styled from "styled-components";
+import { useTable, useRowSelect } from "react-table";
+
+import makeData from "./makeData";
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+
+const IndeterminateCheckbox = React.forwardRef(
+  ({ indeterminate, ...rest }, ref) => {
+    const defaultRef = React.useRef();
+    const resolvedRef = ref || defaultRef;
+
+    React.useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate;
+    }, [resolvedRef, indeterminate]);
+
+    return (
+      <>
+        <input type="checkbox" ref={resolvedRef} {...rest} />
+      </>
+    );
+  }
+);
+
+const CellTextbox = ({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  updateData
+}) => {
+  const [value, setValue] = React.useState(initialValue);
+  const handleChange = (e) => setValue(e.target.value);
+  const handleBlur = () => updateData(index, id, value);
+
+  React.useEffect(() => setValue(initialValue), [initialValue]);
+
+  return <input value={value} onChange={handleChange} onBlur={handleBlur} />;
+};
+
+function Table({ columns, data, updateData, skipReset }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    selectedFlatRows,
+    state: { selectedRowIds }
+  } = useTable(
+    {
+      columns,
+      data,
+      updateData,
+      autoResetSelectedRows: !skipReset
+    },
+    useRowSelect,
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => [
+        // Let's make a column for selection
+        {
+          id: "selection",
+          // The header can use the table's getToggleAllRowsSelectedProps method
+          // to render a checkbox
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <div>
+              <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+            </div>
+          ),
+          // The cell can use the individual row's getToggleRowSelectedProps method
+          // to the render a checkbox
+          Cell: ({ row }) => (
+            <div>
+              <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+            </div>
+          )
+        },
+        ...columns
+      ]);
+    }
+  );
+
+  // Render the UI for your table
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <p>Selected Rows: {Object.keys(selectedRowIds).length}</p>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedRowsIds: selectedRowIds,
+              "selectedFlatRows[].original": selectedFlatRows.map(
+                (d) => d.original
+              )
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+    </>
+  );
+}
+
+function App() {
+  const skipResetRef = React.useRef(false);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        columns: [
+          {
+            Header: "First Name",
+            accessor: "firstName"
+          },
+          {
+            Header: "Last Name",
+            accessor: "lastName"
+          }
+        ]
+      },
+      {
+        Header: "Info",
+        columns: [
+          {
+            Header: "Age",
+            accessor: "age"
+          },
+          {
+            Header: "Visits",
+            accessor: "visits"
+          },
+          {
+            Header: "Status",
+            accessor: "status"
+          },
+          {
+            Header: "Profile Progress",
+            accessor: "progress"
+          },
+          {
+            Header: "Notes",
+            accessor: "notes",
+            Cell: (p) => {
+              return <CellTextbox {...p} />;
+            }
+          }
+        ]
+      }
+    ],
+    []
+  );
+
+  const [data, setData] = React.useState(
+    makeData(10).map((instance) => {
+      const { notes, ...rest } = instance;
+      return {
+        ...rest,
+        notes: notes || ""
+      };
+    })
+  );
+
+  const updateData = (rowIndex, columnId, value) => {
+    skipResetRef.current = true;
+    setData((old) =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value
+          };
+        }
+
+        return row;
+      })
+    );
+  };
+
+  return (
+    <Styles>
+      <Table
+        columns={columns}
+        data={data}
+        updateData={updateData}
+        skipReset={skipResetRef.current}
+      />
+    </Styles>
+  );
+}
+
+export default App;
